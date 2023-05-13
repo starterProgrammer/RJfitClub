bca09f2d6cbe441d4111f75d046b30ed2b21b3c6
starterProgrammer <ali.talaee.engeneer@gmail.com>
2023-05-13 21:48:24 +0330

    plan section completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { Hero } from './components/Hero/Hero';
+import { Journey } from './components/Journey/Journey';
 import { Programms } from './components/Programms/Programms';
 import { Reasons } from './components/Reasons/Reasons';
 function App() {
@@ -8,7 +9,7 @@ function App() {
       <Hero />
       <Programms />
       <Reasons />
-
+      <Journey />
     </div>
   );
 }
---
+++ src/components/Journey/Journey.jsx
@@ -0,0 +1,50 @@
+import './Journey.css'
+import { plansData } from '../../data/plansData'
+import tick from '../../assets/whiteTick.png'
+
+export const Journey = () => {
+    return (
+        <div className="journey">
+            {/* Journey Title */}
+            <div className='journey-titles'>
+                <span className='strok-text'>Ready to Start</span>
+                <span>Your Journey</span>
+                <span className='strok-text'>now withus</span>
+            </div>
+
+            {/* Journey plans */}
+            <div className='juorney-plans'>
+                {plansData.map((plan) => {
+                    return (
+                        <div className='plan'>
+                            {plan.icon}
+                            <span>{plan.name}</span>
+                            <span>
+                                $
+                                {plan.price}
+                            </span>
+                            {
+                                plan.features.map((feature, i) => {
+                                    return (
+                                        <div>
+                                            <div className='features'>
+                                                <img src={tick} alt="" />
+                                                <span key={i}>{feature}</span>
+                                            </div>
+                                        </div>
+                                    )
+                                })
+                            }
+                            <div className='benefit'>
+                                <span>See more benefits -></span>
+                            </div>
+                            <button className='btn'>join now</button>
+
+
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+    )
+}
